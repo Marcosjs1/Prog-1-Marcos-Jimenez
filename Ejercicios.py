0967899ef329c7efda14eb0acd3b9755d9744d8f
Marcos Jimenez <gaston14j@gmail.com>
2023-08-17 10:32:00 -0300

    EjerciciosComplementarios

---
+++ Ejercicios.py
@@ -0,0 +1,58 @@
+"""Ejercicio 1"""
+numero1 = int(5)
+print(numero1)
+"""Ejercicio 2"""
+numero2 = float(8.10)
+print(numero2)
+"""Ejercicio 3"""
+suma = float(numero1 + numero2)
+print(suma)
+"""Ejercicio 4"""
+mult = float(numero1*numero2)
+div = float(numero2/numero1)
+rest = float(numero1-numero2)
+print(mult,div,rest)
+"""Ejercicio 5"""
+nombre = "Marcos"
+print(nombre)
+"""Ejercicio 6"""
+precio = float(60.5)
+print(precio)
+"""Ejercicio 7"""
+descuento = float(0.75)
+print(descuento)
+"""Ejercicio 8"""
+precio_final = float(precio*descuento)
+print(precio_final)
+"""Ejercicio 9"""
+cadena = "Messi es el mejor jugador del mundo"
+print(cadena)
+"""Ejercicio 10"""
+longitud = len(cadena)
+print(len(cadena))
+"""Ejercicio 11"""
+precio = int(80.9)
+print(precio)
+"""Ejercicio 12"""
+nombre = "Marcos "
+apellido = "Jimenez"
+nombre_completo = nombre + apellido
+print(nombre_completo)
+"""Ejecicio 13"""
+edad = int(22)
+edad = 22
+print(edad)
+edad += 5
+print(edad)
+edad -=10
+print(edad)
+"""Ejercicio 14"""
+altura = float(1.81)
+altura_final = (altura*4)/3
+print(altura_final)
+"""Ejercicio 15"""
+nombre_completo ="MARCOS JIMENEZ"
+print(nombre_completo.lower())
+"""Ejercicio 16"""
+nombre_mayus = "MARCOS JIMENEZ"
+print(nombre_mayus.title())
